2e831270ee26ff772f1e4a063e1998ccfebce946
eee8aa <carly.lyddiard@gmail.com>
2016-06-20 15:43:29 +1000

    Blockquote With Footer: Add context menus.
    Clean up when attribution is empty (remove unneeded footer element).

--- blockquoteWithFooter/dialogs/blockquoteWithFooter.js
+++ blockquoteWithFooter/dialogs/blockquoteWithFooter.js
@@ -13,29 +13,85 @@ CKEDITOR.dialog.add('blockquoteWithFooterDialog', function (editor) {
                         type: 'textarea',
                         id: 'quote',
                         label: 'Quote Text',
-                        validate: CKEDITOR.dialog.validate.notEmpty("Quote field cannot be empty.")
+                        validate: CKEDITOR.dialog.validate.notEmpty("Quote field cannot be empty."),
+                        setup: function (element) {
+                            var textNode = element.getChild(0);
+                            if (textNode) {
+                                this.setValue(textNode.getText())
+                            }
+                            else {
+                                this.setValue('');
+                            }
+                            
+                        },
+
+                        commit: function (element) {
+                            element.setText(this.getValue());
+                        }
                     },
                     {
                         type: 'text',
                         id: 'attribution',
                         label: 'Attribution',
+                        setup: function (element) {
+                            var footerElem = element.getChild(1);
+                            if (footerElem) {
+                                this.setValue(footerElem.getText());
+                            }
+                            else {
+                                this.setValue('');
+                            }
+                        },
+
+                        commit: function (element) {
+                            
+                            var footerElem = element.getChild(1);
+                            if (footerElem) {
+                                if (this.getValue()) {
+                                    footerElem.setText(this.getValue());
+                                }
+                                else {
+                                    footerElem.remove();
+                                }
+                            }
+                            else {
+                                if (this.getValue()) {
+                                    var attributionElem = editor.document.createElement('footer');
+                                    attributionElem.setText(this.getValue());
+                                    attributionElem.appendTo(element);
+                                }
+                            }
+                            
+                        }
                     }
                 ]
             }
         ],
+        
+        onShow: function () {
+            var selection = editor.getSelection();
+            var element = selection.getStartElement();
+            if (element)
+                element = element.getAscendant('blockquote', true);
+
+            if (!element || element.getName() != 'blockquote') {
+                element = editor.document.createElement('blockquote');
+                this.insertMode = true;
+            }
+            else
+                this.insertMode = false;
+
+            this.element = element;
+
+            if (!this.insertMode)
+                this.setupContent(element);
+        },
         onOk: function () {
             var dialog = this;
-            var quoteElem = editor.document.createElement('blockquote');
-            quoteElem.setText(dialog.getValueOf('tab-basic', 'quote'));
-            
-            var attribution = dialog.getValueOf('tab-basic', 'attribution');
-            if (attribution) {
-                var attributionElem = editor.document.createElement('footer');
-                attributionElem.setText(attribution);
-                attributionElem.appendTo(quoteElem);
-            }
-
-            editor.insertElement(quoteElem);
+            var quoteElem = this.element;
+            this.commitContent(quoteElem);
+            if (this.insertMode)
+                editor.insertElement(quoteElem);
         }
     };
 });
--- blockquoteWithFooter/plugin.js
+++ blockquoteWithFooter/plugin.js
@@ -1,5 +1,5 @@
 CKEDITOR.plugins.add('blockquoteWithFooter', {
-    icons: 'blockq',
+    icons: 'blockquoteWithFooter',
     init: function (editor) {
         editor.addCommand('blockquoteWithFooter', new CKEDITOR.dialogCommand('blockquoteWithFooterDialog'));
         editor.ui.addButton('BlockquoteWithFooter', {
@@ -7,6 +7,30 @@ CKEDITOR.plugins.add('blockquoteWithFooter', {
             command: 'blockquoteWithFooter',
             toolbar: 'insert'
         });
+        
+        /*Context menus*/
+        if (editor.contextMenu) {
+            
+        }
+
+        if ( editor.contextMenu ) {
+            editor.addMenuGroup('blockqWithFootGroup');
+            editor.addMenuItem('blockquoteItem', {
+                label: 'Edit BlockQuote',
+                icon: this.path + 'icons/blockquoteWithFooter.png',
+                command: 'blockquoteWithFooter',
+                group: 'blockqWithFootGroup'
+            });
+
+            editor.contextMenu.addListener( function( element ) {
+                if ( element.getAscendant( 'blockquote', true ) ) {
+                    return { blockquoteItem: CKEDITOR.TRISTATE_OFF };
+            }
+            });
+        }
+
+
         CKEDITOR.dialog.add('blockquoteWithFooterDialog', this.path + 'dialogs/blockquoteWithFooter.js');
+
     }
 });
